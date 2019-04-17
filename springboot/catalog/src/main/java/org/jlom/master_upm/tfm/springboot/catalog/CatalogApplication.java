e2eccdfa2fbd25ec5560a6eb42c35dfa7c566006
jlojosnegros <jl.ojosnegros.manchon@gmail.com>
2019-04-17 10:30:12 +0200

    Catalog: Initial commit.

---
+++ springboot/catalog/src/main/java/org/jlom/master_upm/tfm/springboot/catalog/CatalogApplication.java
@@ -0,0 +1,13 @@
+package org.jlom.master_upm.tfm.springboot.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CatalogApplication.class, args);
+  }
+
+}
---
+++ springboot/catalog/src/test/java/org/jlom/master_upm/tfm/springboot/catalog/CatalogApplicationTests.java
@@ -0,0 +1,16 @@
+package org.jlom.master_upm.tfm.springboot.catalog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CatalogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
