3df9fc8231f9e1db9d7beddadeba65434e16bb66
jlojosnegros <jl.ojosnegros.manchon@gmail.com>
2019-04-23 21:11:39 +0200

    StreamControl: Model Tests. WIP

--- springboot/stream-control/src/test/java/org/jlom/master_upm/tfm/springboot/stream_control/model/ModelTest.java
+++ springboot/stream-control/src/test/java/org/jlom/master_upm/tfm/springboot/stream_control/model/ModelTest.java
@@ -2,8 +2,11 @@ package org.jlom.master_upm.tfm.springboot.stream_control.model;
 
 
 import org.jlom.master_upm.tfm.springboot.stream_control.model.api.IStreamControlRepository;
+import org.jlom.master_upm.tfm.springboot.stream_control.model.daos.StreamControlData;
+import org.jlom.master_upm.tfm.springboot.stream_control.model.daos.StreamStatus;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +15,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import redis.embedded.RedisServer;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
@@ -43,5 +48,61 @@ public class ModelTest {
   }
 
 
-  
+  @Test
+  public void given_NoData_when_findRunning_then_ShouldNotFindAnything() {
+
+    final long userId = 1;
+    final long deviceId =1;
+
+    StreamControlData streamingRunning = streamControlRepository.findStreamingRunning(userId, deviceId);
+
+    assertThat(streamingRunning).isNull();
+
+  }
+
+  @Test
+  public void given_OneData_when_findRunning_then_ShouldNotFindAnything() {
+
+    StreamControlData OneRunning = StreamControlData.builder()
+            .userId(1)
+            .deviceId(1)
+            .streamId(1)
+            .status(StreamStatus.RUNNING)
+            .build();
+    streamControlRepository.save(OneRunning);
+
+    StreamControlData streamingRunning = streamControlRepository.findStreamingRunning(OneRunning.getUserId()
+            ,OneRunning.getDeviceId());
+
+    assertThat(streamingRunning).isNotNull();
+    assertThat(streamingRunning.getStatus()).isEqualTo(StreamStatus.RUNNING);
+    assertThat(streamingRunning).isEqualTo(OneRunning);
+
+  }
+
+  @Test
+  public void given_SomeData_when_findRunning_then_ShouldNotFindAnything() {
+
+    StreamControlData OneRunning = StreamControlData.builder()
+            .userId(1)
+            .deviceId(1)
+            .streamId(1)
+            .status(StreamStatus.RUNNING)
+            .build();
+    streamControlRepository.save(OneRunning);
+
+    StreamControlData streamingRunning = streamControlRepository.findStreamingRunning(OneRunning.getUserId()
+            ,OneRunning.getDeviceId());
+
+    assertThat(streamingRunning).isEqualTo(OneRunning);
+
+    OneRunning.setStatus(StreamStatus.DONE);
+    streamControlRepository.save(OneRunning);
+
+    streamingRunning = streamControlRepository.findStreamingRunning(OneRunning.getUserId()
+            ,OneRunning.getDeviceId());
+
+    assertThat(streamingRunning).isNull();
+
+  }
 }
