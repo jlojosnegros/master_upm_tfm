ac8a6b2845ece8bf834d02c767995330aa34a495
jlojosnegros <jl.ojosnegros.manchon@gmail.com>
2019-05-14 00:15:25 +0200

    Micronaut Catalog: Almost there with the test. Problems with redis save

--- micronaut/catalog/src/test/java/catalog/view/ViewTest.java
+++ micronaut/catalog/src/test/java/catalog/view/ViewTest.java
@@ -4,28 +4,43 @@ import io.micronaut.context.ApplicationContext;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.runtime.server.EmbeddedServer;
 import io.micronaut.test.annotation.MicronautTest;
+import org.assertj.core.api.Assertions;
+import org.jlom.master_upm.tfm.micronaut.catalog.model.CatalogContent;
+import org.jlom.master_upm.tfm.micronaut.catalog.model.CatalogContentRepository;
+import org.jlom.master_upm.tfm.micronaut.catalog.model.ContentStatus;
+import org.jlom.master_upm.tfm.micronaut.catalog.view.api.dtos.InputCatalogContent;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.jlom.master_upm.tfm.micronaut.catalog.utils.DtosTransformations.serviceToViewContent;
+import static org.jlom.master_upm.tfm.micronaut.catalog.utils.JsonUtils.jsonToList;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @MicronautTest
 public class ViewTest {
 
-
-
-
+  private static final Logger LOG = LoggerFactory.getLogger(ViewTest.class);
 
   private static EmbeddedServer server;
   private static HttpClient client;
 
+  @Inject
+  private CatalogContentRepository repository;
+
+
   @BeforeAll
   public static void setupServer() {
     server = ApplicationContext.run(EmbeddedServer.class);
+
     client = server
             .getApplicationContext()
             .createBean(HttpClient.class, server.getURL());
@@ -43,11 +58,42 @@ public class ViewTest {
 
   @Test
   public void testIssue() throws Exception {
-    String body = client.toBlocking().retrieve("/issues/12");
+
+//    CatalogContentRepository repository = applicationContext.getBean(CatalogContentRepository.class);
+
+    final Date now = Date.from(Instant.now());
+
+
+    CatalogContent expectedContentOne = CatalogContent.builder()
+            .contentId(1)
+            .status(ContentStatus.AVAILABLE)
+            .title("uno")
+            .streamId(1)
+            .available(now)
+            .tags(Set.of("tag1", "tag2"))
+            .build();
+    repository.save(expectedContentOne);
+
+    CatalogContent expectedContentTwo = CatalogContent.builder()
+            .contentId(2)
+            .status(ContentStatus.SOON)
+            .title("dos")
+            .streamId(2)
+            .available(now)
+            .tags(Set.of("tag1", "tag2"))
+            .build();
+    repository.save(expectedContentTwo);
+
+    List<CatalogContent> expectedContents = List.of(expectedContentOne, expectedContentTwo);
+
+
+    String body = client.toBlocking().retrieve("/catalog/content");
     assertNotNull(body);
-    assertEquals(
-            body,
-            "Issue # 12!"
-    );
+
+    var inputCatalogContents = jsonToList(body, InputCatalogContent.class);
+
+    Assertions.assertThat(inputCatalogContents)
+            .containsExactly(serviceToViewContent(expectedContents)
+                    .toArray(new InputCatalogContent[0]));
   }
 }
